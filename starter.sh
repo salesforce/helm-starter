27149c028f7a9619c1d19c3665e3a4064897aa01
Ryan Michela <rmichela@salesforce.com>
2019-08-05 10:48:28 -0400

    License and CoC

--- starter.sh
+++ starter.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Copyright (c) 2018, salesforce.com, inc.
+# All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+# For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+
 set -e
 
 usage() {
